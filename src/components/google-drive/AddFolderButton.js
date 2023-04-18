d0e06dd7a0b7c8a8393c77e25672dc28b2f89290
Lohit Agarwalla <lohit.agarwalla789@gmail.com>
2023-04-19 02:00:15 +0530

    add folder icon

---
+++ src/components/google-drive/AddFolderButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFolderPlus } from "@fortawesome/free-solid-svg-icons"
+
+export default function AddFolderButton() {
+    function openModal()
+    {
+
+    }
+  return (
+    <Button onClick={openModal} variant="outline-success" size="sm">
+        <FontAwesomeIcon icon={faFolderPlus}/>
+      </Button>
+  )
+}
--- src/components/google-drive/Dashboard.js
+++ src/components/google-drive/Dashboard.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import NavbarComponent from './Navbar'
+import { Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFolderPlus } from "@fortawesome/free-solid-svg-icons"
+import AddFolderButton from './AddFolderButton'
 
 export default function Dashboard() {
   return (
     <div>
       <NavbarComponent/>
+      <Container fluid>
+        <AddFolderButton/>
+      </Container>
     </div>
   )
 }
