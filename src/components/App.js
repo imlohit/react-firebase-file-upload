0ed01fd6e1ea271b26b9835f16f110c6973d8fd6
Lohit Agarwalla <lohit.agarwalla789@gmail.com>
2023-04-19 00:58:17 +0530

    add user route

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import Signup from "./authentication/signup"
 import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Dashboard from "./Dashboard"
+import Profile from "./authentication/Profile"
 import Login from "./authentication/login"
 import PrivateRoute from "./authentication/PrivateRoute"
 import ForgotPassword from "./authentication/forgotPassword"
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <AuthProvider>
           <Routes>
-            <Route exact path="/" element={<PrivateRoute><Dashboard/></PrivateRoute>}/>
+            <Route path="/user" element={<PrivateRoute><Profile/></PrivateRoute>}/>
             <Route path="/updateProfile" element={<PrivateRoute><UpdateProfile/></PrivateRoute>}/>
             <Route  path="/signup" element={<Signup/>} />
             <Route  path="/login" element={<Login/>} />
