dc5da136dbb6e76f7e25109a3f07d75067dd53ff
Lohit Agarwalla <lohit.agarwalla789@gmail.com>
2023-04-19 02:12:31 +0530

    add addFolder modal

--- src/components/google-drive/AddFolderButton.js
+++ src/components/google-drive/AddFolderButton.js
@@ -1,16 +1,53 @@
-import React from 'react'
-import { Button } from 'react-bootstrap'
+import React ,{useState} from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFolderPlus } from "@fortawesome/free-solid-svg-icons"
+import { Button, Modal, Form } from "react-bootstrap"
 
 export default function AddFolderButton() {
-    function openModal()
-    {
+    const [open, setOpen] = useState(false)
+    const [name, setName] = useState("")
 
-    }
+    function openModal() {
+        setOpen(true)
+      }
+    
+      function closeModal() {
+        setOpen(false)
+      }
+    
+      function handleSubmit(e) {
+        e.preventDefault()
+        setName("")
+        closeModal()
+        }
   return (
-    <Button onClick={openModal} variant="outline-success" size="sm">
-        <FontAwesomeIcon icon={faFolderPlus}/>
+    <>
+      <Button onClick={openModal} variant="outline-success" size="sm">
+        <FontAwesomeIcon icon={faFolderPlus} />
       </Button>
+      <Modal show={open} onHide={closeModal}>
+        <Form onSubmit={handleSubmit}>
+          <Modal.Body>
+            <Form.Group>
+              <Form.Label>Folder Name</Form.Label>
+              <Form.Control
+                type="text"
+                required
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeModal}>
+              Close
+            </Button>
+            <Button variant="success" type="submit">
+              Add Folder
+            </Button>
+          </Modal.Footer>
+        </Form>
+      </Modal>
+    </>
   )
 }
