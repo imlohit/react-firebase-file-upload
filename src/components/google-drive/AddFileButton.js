72d425c121cf73f561a04553591f26dcd4b426e5
Lohit Agarwalla <lohit.agarwalla789@gmail.com>
2023-04-23 20:21:49 +0530

    add files to database

--- src/components/google-drive/AddFileButton.js
+++ src/components/google-drive/AddFileButton.js
@@ -3,7 +3,7 @@ import { faFileUpload } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useAuth } from "../../contexts/AuthContext"
 import { ROOT_FOLDER } from "../../hooks/useFolder"
-import { storage } from "../../firebase"
+import { storage,database } from "../../firebase"
 
 const AddFileButton = ({ currentFolder }) => {
     const { currentUser } = useAuth()
@@ -20,11 +20,26 @@ const AddFileButton = ({ currentFolder }) => {
       const uploadTask = storage
         .ref(`/files/${currentUser.uid}/${filePath}`)
         .put(file)
+
+        uploadTask.on('state_changed',snapshot=>{
+
+        },()=>{
+
+        },()=>{
+            uploadTask.snapshot.ref.getDownloadURL().then(url=>{
+                database.files.add({
+                    url:url,
+                    name:file.name,
+                    createdAt:database.getCurrentTimestamp(),
+                    folderId:currentFolder.id,
+                    userId:currentUser.uid
+                })
+            })
+        })
     }
     
 
   return (
-    <>
         <label className="btn btn-outline-success btn-sm m-0 mr-2">
         <FontAwesomeIcon icon={faFileUpload} />
         <input
@@ -33,7 +48,6 @@ const AddFileButton = ({ currentFolder }) => {
           style={{ opacity: 0, position: "absolute", left: "-9999px" }}
         />
       </label>
-    </>
   )
 }
 
